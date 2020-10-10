78908e3580b1cb4f6065ed43f07431a405f51952
aaa <1065537421@qq.com>
2020-10-10 17:33:01 +0800

    xiugai1

--- run.py
+++ run.py
@@ -24,7 +24,9 @@ class View:
     def POST(self):
         try:
             token = web.input().token
-            date_data = web.input().date_data
+            # date_data = web.input().date_data
+            date_ts = int(time.time())
+            date_data = time.strftime('%Y%m%d', time.localtime(date_ts))
     
             try:
                 parse_token = jwt.decode(token, 'secret', algorithms='HS256')
@@ -38,20 +40,23 @@ class View:
             sub = username
             act = 'read'
             if e.enforce(sub, dom, obj, act):
-                posts = stock_great_retail.get_datas(date_data)
-                if posts:
-                    d_list = []
-                    for i in posts:
-                        d_dict = OrderedDict()
-                        d_dict['code'] = i.code
-                        d_dict['update_date'] = i.update_date
-                        d_dict['shareholder_falling_count'] = i.shareholder_falling_count
-                        d_dict['sdlu_great_retail_count'] = i.sdlu_great_retail_count
-                        d_dict['float_share'] = i.float_share
-                        d_list.append(d_dict)
-                    return json.dumps({'status': 'success', 'code': 200, 'data': d_list})
-                else:
-                    return json.dumps({'status': 'fail', 'code': 15, 'message': 'no datas'})
+                while True:
+                    posts = stock_great_retail.get_datas(date_data)
+                    if posts:
+                        d_list = []
+                        for i in posts:
+                            d_dict = OrderedDict()
+                            d_dict['code'] = i.code
+                            d_dict['update_date'] = i.update_date
+                            d_dict['shareholder_falling_count'] = i.shareholder_falling_count
+                            d_dict['sdlu_great_retail_count'] = i.sdlu_great_retail_count
+                            d_dict['float_share'] = i.float_share
+                            d_list.append(d_dict)
+                        return json.dumps({'status': 'success', 'code': 200, 'data': d_list})
+                    else:
+                        again_ts = date_ts - 24*3600
+                        date_data = time.strftime('%Y%m%d', time.localtime(again_ts))
+                        # return json.dumps({'status': 'fail', 'code': 15, 'message': 'no datas'})
             else:
                 return json.dumps({'status': 'fail', 'code': 401, 'message': 'unauthorization operation'})
         except Exception as e:
